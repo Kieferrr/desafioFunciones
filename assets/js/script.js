372455f3717aa14cd366f014c8e725e03b4e56e2
Nicolás <nicog.rivero@gmail.com>
2025-05-07 21:30:56 -0400

    Desafio 3 listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
+// Función predeterminada:
 function pintar() {
     ele.style.backgroundColor = 'yellow'
 }
 const ele = document.getElementById("ele1")
 ele.addEventListener("click", pintar);
+
+// Función 2.2
+function pintar(elemento) {
+    elemento.style.backgroundColor = 'yellow'
+}
+ele.addEventListener("click", function (event) {
+    pintar(event.target)
+});
+
+// Funcion 2.3
+function pintar(elemento, color = "green") {
+    elemento.style.backgroundColor = color
+}
+
+ele.addEventListener("click", function (event) {
+    pintar(event.target, 'yellow')
+});
+
